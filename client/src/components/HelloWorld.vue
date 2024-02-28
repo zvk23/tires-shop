bb8fd2bce6bded540d0d49d54f7606533f379b6e
denex28 <ms.tiesto@mail.ru>
2024-02-29 02:46:37 +0300

    кнопки

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,35 +1,11 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div>залупа</div>
+  <h1>пупа</h1>
+  <button type="button" name="НОВИНКА" class="NEW">НОВИНКА
+  </button>
+  <button type="button" name="хит" class="hit">ХИТ</button>
+  <button type="button" name="РАСПРОДАЖА" class="sale but">РАСПРОДАЖА</button>
+  <button type="button" name="-20" class="ss">-20%</button>
 </template>
 
 <script lang="ts">
@@ -47,7 +23,29 @@ export default defineComponent({
 <style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
+} 
+button {
+  font-size: 11px;
+  color:white;
+  text-align:center;
+  font-weight: 500;
+  padding: 10px 10px;
+  margin:10px 10px;
+  font-family:sans-serif;
+  border-style: none;
 }
+.NEW { 
+background-color:black ;
+}
+.hit {background-color:#3F71BC ; 
+
+}
+.sale {background-color:#9747FF ;
+}
+.ss {background-color:
+#FF6915;
+}
+
 ul {
   list-style-type: none;
   padding: 0;
